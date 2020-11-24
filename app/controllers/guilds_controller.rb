71a90583e5b4cd63007ff60c2c29cee2cdc57568
Elise <elisztomania@gmail.com>
2020-11-24 12:11:55 +0100

    controller methods

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,2 +1,32 @@
 class GuildsController < ApplicationController
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+
+  def set_guild
+
+  end
+
+  def guild_params
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-    resources :quests, only: [:index, :show, :create, :new, :edit, :update]
+    resources :quests, only: [:index, :show, :create, :new, :edit, :update] do
+      resources :guild, only: [:show, :create, :new, :edit, :update]
+    end
 
 end
