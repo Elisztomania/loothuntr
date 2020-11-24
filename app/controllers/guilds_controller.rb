9596ce0ef3e5c799b9270d8dbc83bab967d750eb
Elise <elisztomania@gmail.com>
2020-11-24 14:42:38 +0100

    methods in controller

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,32 +1,42 @@
 class GuildsController < ApplicationController
+  before_action :set_guild, only: [:show, :edit, :update]
 
   def show
-
+    # show dashboard with the chat, the calendar, th members of the team
   end
 
   def new
-
+    @guild = Guild.new
   end
 
   def create
-
+    @guild = Guild.new(guild_params)
+    @guild.creator = current_user
+    if @guild.save
+      redirect_to @guild
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @guild.update(guild_params)
+      redirect_to @guild, notice: 'Votre quête est bien modifiée ✌️'
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_guild
-
+    @guild = Guild.find(params[:id])
   end
 
   def guild_params
-
+    params.require(:guild).permit(:quest_id, :creator, :name, :description)
   end
 end
