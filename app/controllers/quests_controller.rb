29630d6f3c6adb3c901f1b72a97b18c174540df3
Huy Vu	Nguyen <hv.ng@outlook.fr>
2020-11-24 14:26:44 +0100

    create the quests controller fuck yeeee

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,36 +1,47 @@
 class QuestsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_quest, only: [:show, :edit, :update]
 
   def index
-
+    @quests = Quest.all
   end
 
   def new
-
+    @quest = Quest.new
   end
 
   def show
-
+    # @favorite = @mushroom.favorites.find_by(user: current_user)
   end
 
   def create
-
+    @quest = Quest.new(quest_params)
+    @quest.user = current_user
+    if @quest.save
+      redirect_to @quest
+    else
+      render :new
+    end
   end
 
   def edit
-
   end
 
   def update
-
+    if @quest.update(quest_params)
+      redirect_to @quest, notice: 'Votre quête est bien modifié 🤠'
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_quest
+    @quest = Quest.find(params[:id])
   end
 
   def quest_params
-
+    params.require(:quest).permit(:title, :description, :location, :resolved, :creator_id)
   end
 end
