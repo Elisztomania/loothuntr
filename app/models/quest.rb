94dcc920f0851874b447cb4976ad1e65f39e12ae
Gagou <gaelle.shearn.jmr@gmail.com>
2020-11-24 14:41:08 +0100

    seed avec guild

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -2,7 +2,7 @@ class Quest < ApplicationRecord
   belongs_to :creator, class_name: 'User'
 
   validates :title, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimun: 30 }
+  # validates :description, presence: true, length: { minimun: 30 }
   validates :location, presence: true
   validates :creator, presence: true
 
