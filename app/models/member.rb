10977ed1451731849a6e0430169fd8255093f3a9
Elise <elisztomania@gmail.com>
2020-11-24 12:24:29 +0100

    routes + validates

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,6 @@
 class Member < ApplicationRecord
   belongs_to :user
   belongs_to :guild
+
+  validates :cover_letter, presence: true, length: { in: 20..200 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
     resources :quests, only: [:index, :show, :create, :new, :edit, :update] do
-      resources :guild, only: [:show, :create, :new, :edit, :update]
+      resources :guilds, only: [:show, :create, :new, :edit, :update] do
+        resources :members, only: [:new, :create]
+      end
     end
 
 end
