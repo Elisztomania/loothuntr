e53407a9c2710ad084f60ed11fbe5d1ef73a00e6
Elise <elisztomania@gmail.com>
2020-11-26 19:58:09 +0100

    seeds photo done

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -3,6 +3,7 @@ class Quest < ApplicationRecord
   has_one :guild
   has_many :posts, through: :guild
   has_many :avancees, -> { where(category: 'avancee') }, through: :guild, source: :posts
+  has_one_attached :photo
 
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, length: { minimum: 30 }
