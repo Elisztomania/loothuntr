7ec682b8c3d85d531e3b447c2018f3636337dbc5
Huy Vu	Nguyen <hv.ng@outlook.fr>
2020-11-26 14:35:45 +0100

    adding some views comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,17 @@ Rails.application.routes.draw do
     resources :guilds, only: [:create, :new]
     resources :posts, only: [:edit, :update]
   end
+
   resources :guilds, only: [:show, :edit, :update] do
-    resources :posts, only: [:new, :create]
+    resources :posts, only: [:new, :create] do
+      resources :comments, only: [:create, :new, :edit, :update]
+        end
     resources :members, only: [:new, :create] do
       get '/accepted', to: 'members#accepted'
       get '/refused', to: 'members#refused'
     end
   end
+
+  resources :comments, only: :destroy
 
 end
