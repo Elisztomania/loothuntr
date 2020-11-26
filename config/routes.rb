63257e47458d8a53d3b7f784f3defda9ffcafa88
Gagou <gaelle.shearn.jmr@gmail.com>
2020-11-26 14:35:34 +0100

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :quests, only: [:index, :show, :create, :new, :edit, :update] do
     resources :guilds, only: [:create, :new]
-    resources :posts, only: [:edit, :update]
   end
   resources :guilds, only: [:show, :edit, :update] do
-    resources :posts, only: [:new, :create]
-    resources :members, only: [:new, :create] do
-      get '/accepted', to: 'members#accepted'
-      get '/refused', to: 'members#refused'
+    resources :posts, only: [:new, :create, :edit, :update] do
+      collection do
+        get :pistes
+      end
+      resources :members, only: [:new, :create] do
+        get '/accepted', to: 'members#accepted'
+        get '/refused', to: 'members#refused'
+      end
     end
   end
 
