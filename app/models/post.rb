dadf2d97e97fb03d3f0df5887649a4f6b143eb7c
Gagou <gaelle.shearn.jmr@gmail.com>
2020-11-26 14:27:49 +0100

    model ok

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :guild
 
+  CATEGORY = ["orga", "piste", "avancee"]
+
   validates :title, presence: true
   validates :description, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGROY }
 end
