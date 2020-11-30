87fc64f31a106ea94fc0c13f231252d12b9384a2
Bastien Pagneux <pagneuxbastien@hotmail.fr>
2020-11-30 10:16:02 +0100

    add map v3

--- app/javascript/components/init_mapbox.js
+++ app/javascript/components/init_mapbox.js
@@ -18,11 +18,11 @@ const initMapbox = () => {
     const markers = JSON.parse(mapElement.dataset.markers);
     if (markers) {
     markers.forEach((marker) => {
-      // const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
 
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
-        // .setPopup(popup)
+        .setPopup(popup)
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
